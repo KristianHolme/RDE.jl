2b91f694db80ea48cee22328bcf1e02a810edaed
Kristian Holme <holme.kristian@gmail.com>
2024-10-16 18:18:59 +0200

    modified type testing

--- test/types_tests.jl
+++ test/types_tests.jl
@@ -1,12 +1,16 @@
 @test begin
-    d = RDE.create_dealiasing_vector(16, Float32)
+    params = RDEParam{Float32}(;N=16)
+    d = RDE.create_dealiasing_vector(params)
     typeof(d) == Vector{Float32}
 end
 
 @test begin
-    ik, k2 = RDE.create_spectral_derivative_arrays(16, Float32)
+    params = RDEParam{Float32}(;N=16)
+    ik, k2 = RDE.create_spectral_derivative_arrays(params)
+    @show typeof(ik), typeof(k2)
     typeof(ik) == Vector{ComplexF32} && typeof(k2) == Vector{Float32}
 end
+
 
 @test begin
     env = RDEEnv{Float32}()
