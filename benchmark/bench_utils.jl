dae3b4b76fa53b6165d1a32cd878e2aa710822e2
Kristian Holme <holme.kristian@gmail.com>
2026-02-04 14:17:18 +0100

    add benchmarking

---
+++ benchmark/bench_utils.jl
@@ -0,0 +1,104 @@
+module BenchUtils
+
+using RDE
+const DEFAULT_N = 256
+const DEFAULT_TMAX = 5.0f0
+const DEFAULT_SAMPLES = 200
+const DEFAULT_SAVEFRAMES = 5
+const DEFAULT_SPATIAL_WIDTH = 9
+
+function make_params(; T = Float32, N = DEFAULT_N, tmax = DEFAULT_TMAX)
+    params = RDEParam{T}(N = N, tmax = tmax)
+    return params
+end
+
+function make_problem(;
+        T = Float32,
+        N = DEFAULT_N,
+        tmax = DEFAULT_TMAX,
+        reset_strategy = Default(),
+        control_shift_strategy = ZeroControlShift(),
+        method = FiniteVolumeMethod{T}()
+    )
+    params = make_params(; T = T, N = N, tmax = tmax)
+    prob = RDEProblem(
+        params;
+        reset_strategy = reset_strategy,
+        method = method,
+        control_shift_strategy = control_shift_strategy
+    )
+    return prob
+end
+
+function setup_solve_problem(; T = Float32, N = DEFAULT_N, tmax = DEFAULT_TMAX)
+    prob = make_problem(; T = T, N = N, tmax = tmax)
+    return prob
+end
+
+function setup_rhs(; T = Float32, N = DEFAULT_N, tmax = DEFAULT_TMAX)
+    prob = make_problem(; T = T, N = N, tmax = tmax)
+    uλ = vcat(prob.u0, prob.λ0)
+    duλ = similar(uλ)
+    t = zero(T)
+    return prob, uλ, duλ, t
+end
+
+function setup_shock_inputs(; T = Float32, N = DEFAULT_N)
+    prob = make_problem(; T = T, N = N, tmax = DEFAULT_TMAX)
+    dx = get_dx(prob)
+    u = copy(prob.u0)
+    return u, dx
+end
+
+function setup_control_shift(;
+        T = Float32,
+        N = DEFAULT_N,
+        tmax = DEFAULT_TMAX,
+        width_points = DEFAULT_SPATIAL_WIDTH
+    )
+    control_shift_strategy = LinearControlShift(T(1))
+    prob = make_problem(
+        ; T = T,
+        N = N,
+        tmax = tmax,
+        control_shift_strategy = control_shift_strategy
+    )
+    cache = prob.method.cache
+    set_spatial_control_smoothing!(cache, width_points)
+    u = prob.u0
+    t = T(2) * cache.dx
+    return cache, control_shift_strategy, u, t
+end
+
+function setup_smooth_control(; T = Float32, N = DEFAULT_N)
+    target = zeros(T, N)
+    current = fill(T(2), N)
+    previous = fill(T(1), N)
+    t = T(0.5)
+    control_t = zero(T)
+    τ_smooth = one(T)
+    return target, t, control_t, current, previous, τ_smooth
+end
+
+function setup_smooth_spatial(;
+        T = Float32,
+        N = DEFAULT_N,
+        width_points = DEFAULT_SPATIAL_WIDTH
+    )
+    prob = make_problem(; T = T, N = N, tmax = DEFAULT_TMAX)
+    cache = prob.method.cache
+    set_spatial_control_smoothing!(cache, width_points)
+    target = copy(cache.s_t)
+    scratch = cache.spatial_scratch
+    kernel = cache.spatial_kernel
+    return target, scratch, kernel
+end
+
+function setup_cfl_dtFE(; T = Float32, N = DEFAULT_N, tmax = DEFAULT_TMAX)
+    prob = make_problem(; T = T, N = N, tmax = tmax)
+    uλ = vcat(prob.u0, prob.λ0)
+    t = zero(T)
+    return uλ, prob, t
+end
+
+end
---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,76 @@
+using BenchmarkTools
+using OrdinaryDiffEq
+using RDE
+
+include("bench_utils.jl")
+using .BenchUtils
+
+const SUITE = BenchmarkGroup()
+
+solver = BenchmarkGroup()
+SUITE["solver"] = solver
+
+solver["solve_pde"] = @benchmarkable begin
+    solve_pde!(
+        prob;
+        saveframes = BenchUtils.DEFAULT_SAVEFRAMES,
+        alg = OrdinaryDiffEq.SSPRK33(),
+        adaptive = false
+    )
+end setup = begin
+    prob = BenchUtils.setup_solve_problem()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+rhs = BenchmarkGroup()
+SUITE["rhs"] = rhs
+
+rhs["RDE_RHS!"] = @benchmarkable begin
+    RDE_RHS!(duλ, uλ, prob, t)
+end setup = begin
+    prob, uλ, duλ, t = BenchUtils.setup_rhs()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+control = BenchmarkGroup()
+SUITE["control"] = control
+
+control["update_control_shifted!"] = @benchmarkable begin
+    RDE.update_control_shifted!(cache, control_shift_strategy, u, t)
+end setup = begin
+    cache, control_shift_strategy, u, t = BenchUtils.setup_control_shift()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+control["smooth_control!"] = @benchmarkable begin
+    RDE.smooth_control!(target, t, control_t, current, previous, τ_smooth)
+end setup = begin
+    target, t, control_t, current, previous, τ_smooth = BenchUtils.setup_smooth_control()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+control["smooth_spatial!"] = @benchmarkable begin
+    RDE.smooth_spatial!(target, scratch, kernel)
+end setup = begin
+    target, scratch, kernel = BenchUtils.setup_smooth_spatial()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+cfl = BenchmarkGroup()
+SUITE["cfl"] = cfl
+
+cfl["cfl_dtFE"] = @benchmarkable begin
+    RDE.cfl_dtFE(uλ, prob, t)
+end setup = begin
+    uλ, prob, t = BenchUtils.setup_cfl_dtFE()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+utils = BenchmarkGroup()
+SUITE["utils"] = utils
+
+utils["shock_locations"] = @benchmarkable begin
+    shock_locations(u, dx)
+end setup = begin
+    u, dx = BenchUtils.setup_shock_inputs()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
+
+utils["shock_indices"] = @benchmarkable begin
+    shock_indices(u, dx)
+end setup = begin
+    u, dx = BenchUtils.setup_shock_inputs()
+end evals = 1 samples = BenchUtils.DEFAULT_SAMPLES
